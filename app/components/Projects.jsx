56471bcfd4121d053aa919c49dcf1a82632ea977
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2024-03-27 08:20:42 +0600

    bug

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -48,7 +48,7 @@ export default function Projects() {
             transition={{ duration: 1 }}
             className="text-center mt-2 max-w-2xl w-full"
           >
-           These are my all works that I have done
+            These are my all works that I have done
           </motion.h1>
         </div>
         <div className="grid grid-cols-1 items-center mt-28">
@@ -57,41 +57,46 @@ export default function Projects() {
               key={index}
               className="app-light dark:app-dark dark:hover:app-hover hover:app-hover transition-all px-4 relative group duration-500"
             >
-           
               <div className="overflow-hidden rounded-2xl transition-all group-hover:duration-500 relative  flex flex-col md:flex-row items-center">
                 <div className="flex relative flex-col gap-2 justify-end md:justify-center rounded-2xl  items-start md:px-6 py-6 text-white md:text-slate-300 group-hover:text-white w-full h-full">
-               
-                <div className="flex justify-between gap-2 items-start w-full">
-                <h3 className="text-xl lg:text-7xl flex flex-wrap md:uppercase font-semibold">
-                    {service.title}
-                  </h3>
-                <Link href={`/productdetails/${service.id}`}  className="px-4 md:hidden py-2 flex justify-center items-center bg-primary rounded-full text-xs">
-                    
-                      <GoArrowUpRight className="text-white text-lg"/>
+                  <div className="flex justify-between gap-2 items-start w-full">
+                    <h3 className="text-xl lg:text-7xl flex flex-wrap md:uppercase font-semibold">
+                      {service.title}
+                    </h3>
+                    <Link
+                      href={`/productdetails/${service.id}`}
+                      className="px-4 md:hidden py-2 flex justify-center items-center bg-primary rounded-full text-xs"
+                    >
+                      <GoArrowUpRight className="text-white text-lg" />
                     </Link>
-                </div>
-                
+                  </div>
+
                   <div className="flex items-center  flex-wrap  w-full transition-all duration-1000 md:mt-5 gap-2 md:gap-5">
                     {service.subtitle.split(",").map((data) => (
                       <p className="text-xs bg-white/20  rounded-full px-4 py-2 md:px-4 md:py-3">
                         {data}
-                     
                       </p>
                     ))}
-                  
                   </div>
-         
                 </div>
-            
+
                 <div className=" relative group rounded-2xl py-5">
-                <Link href={`/productdetails/${service.id}`}  className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden lg:group-hover:flex z-20 p-12 bg-black/20  dark:bg-slate-800/30 backdrop-blur-lg rounded-full  text-2xl  cursor-pointer">
-                      <GoArrowUpRight className="text-white text-2xl"/>
-                    </Link>
-                
+                  <Link
+                    href={`/productdetails/${service.id}`}
+                    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden lg:group-hover:flex z-20 p-9 bg-black/20  dark:bg-slate-800/30 backdrop-blur-lg rounded-full  text-xl  cursor-pointer"
+                  >
+                    <GoArrowUpRight className="text-white text-xl" />
+                  </Link>
+
                   <Image
-                    className="min-h-full max-w-2xl w-full rounded-xl group-hover:scale-90 transition duration-500"
+                    className="min-h-full max-w-sm w-full rounded-xl group-hover:scale-90 transition duration-500"
                     src={service.icons}
-                    loading="lazy"  objectFit="cover" objectPosition="center" width={1000} height={1000} alt="img"
+                    loading="lazy"
+                    objectFit="cover"
+                    objectPosition="center"
+                    width={1000}
+                    height={1000}
+                    alt="img"
                   />
                 </div>
               </div>
